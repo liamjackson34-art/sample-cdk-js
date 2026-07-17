abe4be1e858bd250dc71cf98bf6176820ff2e794
Liam Jackson <liamjackson34@gmail.com>
2026-07-17 23:31:07 +0100

    Initial commit

---
+++ test/liam-cdk-js.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib/core');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const LiamCdkJs = require('../lib/liam-cdk-js-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/liam-cdk-js-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new LiamCdkJs.LiamCdkJsStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
